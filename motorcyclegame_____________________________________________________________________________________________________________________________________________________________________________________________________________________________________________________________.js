4d441250bbe6652554f002c09fc85862ee13b7d3
snakemoddingiscoolandthisisalongname <92934046+snakemoddingiscoolandthisisalongname@users.noreply.github.com>
2022-03-05 15:04:29 -0500

    Update and rename motorcyclegame____________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________.js to motorcyclegame_____________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________.js

--- motorcyclegame_____________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________.js
+++ motorcyclegame_____________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________.js
@@ -80,6 +80,6 @@ function loop() {
   player.draw();
   requestAnimationFrame(loop);
 }
-onkeydown = (d) => (k[d.key] = 91);
+onkeydown = (d) => (k[d.key] = 0);
 onkeyup = (d) => (k[d.key] = 0);
 loop();
